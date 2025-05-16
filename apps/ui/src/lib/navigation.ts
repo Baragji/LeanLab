31bbc58a05e60e8a2cd27da33824579fdeac675b
Baragji <ybaragji@gmail.com>
2025-05-16 10:58:31 +0200

    Initialize feat/14

---
+++ apps/ui/src/lib/navigation.ts
@@ -0,0 +1,18 @@
+import { env } from "@/env.mjs"
+import { createSharedPathnamesNavigation } from "next-intl/navigation"
+
+import { locales } from "./i18n"
+
+// https://next-intl-docs.vercel.app/docs/routing/navigation
+export const { Link, redirect, usePathname, useRouter } =
+  createSharedPathnamesNavigation({ locales })
+
+export function isAppLink(link: string): boolean {
+  try {
+    const baseUrl = env.NEXT_PUBLIC_APP_PUBLIC_URL ?? ""
+    const url = new URL(link, baseUrl)
+    return url.hostname === new URL(baseUrl).hostname
+  } catch (error) {
+    return false
+  }
+}
